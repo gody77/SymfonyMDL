15e98a92858b76bb5ce6ba2c8f52e3e3db3fd1ca
Godefroi <cgodefroi@yahoo.fr>
2013-03-20 20:36:47 +0100

    ajout de OrganismeType.php dans MdlBundle/Form/Type

---
+++ src/Lam/MdlBundle/Form/Type/OrganismeType.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Description of OrganismeType
+ *
+ * @author Godefroi
+ */
+namespace Lam\MdlBundle\Form\Type;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilder;
+use Lam\MdlBundle\Entity\organisme;
+
+class OrganismeType extends AbstractType
+{
+    public function buildForm(FormBuilder $builder, array $options){
+        $builder->add('id','text',array('label' => "Numéro : "))
+                ->add('nom','text',array('label' => "Nom : "))
+                ->add('prenom','text',array('label' => "Prénom : "))
+                ->add('login','text',array('label' => "Login : "))
+                ->add('mdp','text',array('label' => "Mot de passe : "))
+                ->add('adresse','text',array('label' => "Adresse : "))
+                ->add('cp','integer',array('label' => "CP : "))
+                ->add('ville','text',array('label' => "Ville : ", 'required'=>false,"trim"=>TRUE))
+                ->add('dateEmbauche','date',array('label' => "Date d'embauche : "))
+                ->add('laCategorie');
+    }
+    
+    public function getName(){
+        return 'CG_GsbBundle_organismeType';
+    }
+}
+
+?>
